5b719a7df6ddc68891420ed02dd394270b94a6aa
Burdy <burdy.goureige.ep@whoz.com>
2024-12-04 17:36:58 +0100

    comment server

--- api/index.js
+++ api/index.js
@@ -104,12 +104,12 @@ app.use('/', (req, res) => {
     }
   });
   
-  if (process.env.NODE_ENV !== 'production') { 
+//   if (process.env.NODE_ENV !== 'production') { 
 
-  app.listen(process.env.SERVER_PORT, () => {
-    console.log(`L'application écoute sur http://localhost:${process.env.SERVER_PORT}`);
-  });
-}
+//   app.listen(process.env.SERVER_PORT, () => {
+//     console.log(`L'application écoute sur http://localhost:${process.env.SERVER_PORT}`);
+//   });
+// }
   
 export default app;
 
